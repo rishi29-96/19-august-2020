2b947815aad1a18764c012f533fc88a8f95908c1
rishi29-96 <69304648+rishi29-96@users.noreply.github.com>
2020-08-19 15:28:49 +0530

    Add files via upload

---
+++ Newclass.java
@@ -0,0 +1,9 @@
+public class Newclass{
+public static void main(String[] args)
+{
+int time = 20;
+String result;
+result = (time < 18) ? "good day." : "good evening.";
+System.out.println(result);
+}
+}
